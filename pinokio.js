cec8b36ad8469b05c140b7e3545c8115f19f79f7
cocktailpeanut <cocktailpeanuts@proton.me>
2024-07-22 08:46:48 -0400

    update

--- pinokio.js
+++ pinokio.js
@@ -2,7 +2,7 @@ const path = require('path')
 module.exports = {
   version: "2.0",
   title: "LivePortrait",
-  description: "",
+  description: "Bring portraits to life! https://github.com/KwaiVGI/LivePortrait",
   icon: "icon.png",
   menu: async (kernel, info) => {
     let installed = info.exists("app/env")
