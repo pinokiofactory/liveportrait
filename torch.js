fda53c94d2d254cb0b2af3bf1f77e26b910efac4
cocktailpeanut <cocktailpeanuts@proton.me>
2024-07-22 16:10:16 -0400

    update

--- torch.js
+++ torch.js
@@ -62,7 +62,7 @@ module.exports = {
     },
     // linux cpu
     {
-      "when": "{{platform === 'linux' && (gpu !== 'amd' && gpu !=='amd')}}",
+      "when": "{{platform === 'linux' && (gpu !== 'amd' && gpu !=='nvidia')}}",
       "method": "shell.run",
       "params": {
         "venv": "{{args && args.venv ? args.venv : null}}",
