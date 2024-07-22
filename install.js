d3d358ad5a6e07c58f0e4d3e3ae721f6aa78073c
cocktailpeanut <cocktailpeanuts@proton.me>
2024-07-22 15:37:32 -0400

    remove torch

--- install.js
+++ install.js
@@ -5,7 +5,7 @@ module.exports = {
       method: "shell.run",
       params: {
         message: [
-          "git clone https://github.com/KwaiVGI/LivePortrait app",
+          "git clone https://github.com/cocktailpeanut/LivePortrait app",
         ]
       }
     },
