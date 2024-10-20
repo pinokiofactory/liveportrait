ad010135ebb2d3f49c02d6311e449c2426374d91
cocktailpeanut <cocktailpeanuts@proton.me>
2024-10-19 21:20:30 -0400

    update

--- win.js
+++ win.js
@@ -4,8 +4,13 @@ module.exports = {
     params: {
       message: [
         "conda install -y -c conda-forge 7zip huggingface_hub",
-        "huggingface-cli download cleardusk/LivePortrait-Windows LivePortrait-Windows-v20240829.zip --local-dir {{cwd}}"
+        "huggingface-cli download cleardusk/LivePortrait-Windows LivePortrait-Windows-v20240829.zip --local-dir {{cwd}}",
       ]
+    }
+  }, {
+    method: "process.wait",
+    params: {
+      sec: 5
     }
   }, {
     method: "shell.run",
