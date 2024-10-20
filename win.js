f89a1dd94024a4f1e7cd9e779cf36f574f967169
cocktailpeanut <cocktailpeanuts@proton.me>
2024-10-19 20:57:43 -0400

    update

--- win.js
+++ win.js
@@ -1,16 +1,17 @@
 module.exports = {
   run: [{
-    method: "fs.download",
+    method: "shell.run",
     params: {
-      uri: "https://huggingface.co/cleardusk/LivePortrait-Windows/resolve/main/LivePortrait-Windows-v20240829.zip?download=true"
-      path: "installer.zip"
+      message: [
+        "conda install -y -c conda-forge 7zip huggingface_hub",
+        "huggingface-cli download cleardusk/LivePortrait-Windows LivePortrait-Windows-v20240829.zip --local-dir {{cwd}}"
+      ]
     }
   }, {
     method: "shell.run",
     params: {
       message: [
-        "conda install -y -c conda-forge 7zip",
-        "7z x installer.zip",
+        "7z x LivePortrait-Windows-v20240829.zip",
         "move LivePortrait-Windows-v20240829 app",
         "del installer.zip"
       ]
